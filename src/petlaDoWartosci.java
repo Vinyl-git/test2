1c90eafef267b5100c956e6cb6148184d6f100fd
Vinyl-git <contact.marcinblazejewski@gmail.com>
2020-11-21 22:57:46 +0100

    test_doPodanejWartosci

---
+++ src/petlaDoWartosci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class petlaDoWartosci {
+    public static void main(String[] args) {
+
+        System.out.println("Wpisz wartość: ");
+
+        Scanner userInput = new Scanner(System.in);
+        int userNumber = userInput.nextInt();
+
+
+        for (int x=1; x<=userNumber; x++) {
+            if (x%2 !=1) {
+                System.out.println(x);
+            }
+        }
+    }
+}

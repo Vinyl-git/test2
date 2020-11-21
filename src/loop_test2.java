041e93d36807c3aec2579232b121c2d05eae056e
Vinyl-git <contact.marcinblazejewski@gmail.com>
2020-11-21 22:27:43 +0100

    test_petlaWhile

---
+++ src/loop_test2.java
@@ -0,0 +1,9 @@
+public class loop_test2 {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<=10) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
